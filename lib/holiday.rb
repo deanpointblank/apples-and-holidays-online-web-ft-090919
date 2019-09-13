808262ba5d09a386b5442f3fb65c156ee7cda61e
Dean L Betty <deanlloyd64@gmail.com>
2019-09-13 23:21:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holiday|
+    binding.pry
     print seasons.to_s
   end
 end
