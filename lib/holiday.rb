b884428adc73a239075f66125985b52268eceeee
Dean L Betty <deanlloyd64@gmail.com>
2019-09-13 23:50:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
+      binding.pry
       puts " #{holiday.to_s.capitalize!}:  #{supplies.to_s.capitalize!}"
     end
   end
