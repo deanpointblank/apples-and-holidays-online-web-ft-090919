95048dd994a61a07b9aa4430e3fb26040e668fa7
Dean L Betty <deanlloyd64@gmail.com>
2019-09-14 00:53:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,8 +83,9 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supplies|
       holiday_name = holiday.to_s.split("_").join(" ").capitalize
       if holiday_name.split(" ").length > 1
+        tempoary_name = hoilday_name.split(" ")
         binding.pry
-        holiday_name.split(" ").map do |individual_word|
+        tempoary_name.map do |individual_word|
           holiday_name_array << individual_word.capitalize!
         end
         holiday_name = holiday_name_array.join(" ")
